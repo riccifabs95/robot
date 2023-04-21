f80655c3b56baf66af35b5262360a5451f90ca6b
Fabio Ricci <riccifabio95@gmail.com>
2023-04-21 19:03:27 +0200

    Ciao

--- test.py
+++ test.py
@@ -1,1 +1,24 @@
+# Python program to explain cv2.imshow() method
+
+# importing cv2
 import cv2
+
+# path
+path = r'C:\Users\faricci\OneDrive - Fondazione Istituto Italiano Tecnologia\Desktop\robot-1\input\image_2.jpg'
+
+# Reading an image in default mode
+image = cv2.imread(path)
+
+# Window name in which image is displayed
+window_name = 'image'
+
+# Using cv2.imshow() method
+# Displaying the image
+cv2.imshow(window_name, image)
+
+# waits for user to press any key
+# (this is necessary to avoid Python kernel form crashing)
+cv2.waitKey(0)
+
+# closing all open windows
+cv2.destroyAllWindows()
